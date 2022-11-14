1358162ee129cd7aebdf2a2e10e337824b40c361
Michele Rossi <m.rossi@overside.it>
2022-11-14 23:54:20 +0100

    add state machine

---
+++ utils/state.gd
@@ -0,0 +1,22 @@
+# Virtual base class for all states.
+class_name State
+extends Node
+
+var state_machine = null
+
+func enter(_msg := {}) -> void:
+	pass
+
+func handle_input(_event: InputEvent) -> void:
+	pass
+
+
+func update(_delta: float) -> void:
+	pass
+
+
+func physics_update(_delta: float) -> void:
+	pass
+
+func exit() -> void:
+	pass
---
+++ utils/state_machine.gd
@@ -0,0 +1,36 @@
+class_name StateMachine
+extends Node
+
+signal transitioned(state_name)
+
+# Path to the initial active state. We export it to be able to pick the initial state in the inspector.
+export var initial_state := NodePath()
+
+onready var state: State = get_node(initial_state)
+
+func _ready() -> void:
+    # Wait untill the parent emits ready signal
+    # The states are children of the owner node so their `_ready()` callback will execute first.
+	yield(owner, "ready")
+
+	for child in get_children():
+		child.state_machine = self
+	state.enter()
+
+func _unhandled_input(event: InputEvent) -> void:
+	state.handle_input(event)
+
+func _process(delta: float) -> void:
+	state.update(delta)
+
+func _physics_process(delta: float) -> void:
+	state.physics_update(delta)
+
+func transition_to(target_state_name: String, msg: Dictionary = {}) -> void:
+	if not has_node(target_state_name):
+		return
+
+	state.exit()
+	state = get_node(target_state_name)
+	state.enter(msg)
+	emit_signal("transitioned", state.name)
