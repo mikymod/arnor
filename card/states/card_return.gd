a5ee23d3f578a4a84f31143637b31292116687dc
Michele Rossi <m.rossi@overside.it>
2022-12-06 00:39:28 +0100

    cleanup

--- card/states/card_return.gd
+++ card/states/card_return.gd
@@ -1,12 +1,12 @@
 class_name CardReturn
 extends State
 
-export var root_path: NodePath
-onready var root: Node2D = get_node(root_path)
+export var card_path: NodePath
+onready var card: Node2D = get_node(card_path)
 
 func process(_delta: float) -> void:
-	root.global_position = lerp(root.global_position, root.init_pos, 0.1);
-	if root.global_position.distance_to(root.init_pos) <= 0.01:
+	card.global_position = lerp(card.global_position, card.init_pos, 0.1);
+	if card.global_position.distance_to(card.init_pos) <= 0.01:
 		state_machine.transition_to('Idle')
 
 func _on_Card_input_event(event: InputEvent) -> void:
--- card/states/card_selected.gd
+++ card/states/card_selected.gd
@@ -1,15 +1,15 @@
 class_name CardSelected
 extends State
 
-export var root_path: NodePath
-onready var root: Node2D = get_node(root_path)
+export var card_path: NodePath
+onready var card: Node2D = get_node(card_path)
 
 var mouse_pos: Vector2
 
 func process(_delta: float) -> void:
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		mouse_pos = get_global_mouse_position()
-		root.global_position = mouse_pos
+		card.global_position = mouse_pos
 	else:
 		if state_machine.place != null:
 			state_machine.transition_to('Play')
