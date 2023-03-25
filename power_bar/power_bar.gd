81798655345aa38eb7d7af2adf948bea4c305f16
Michele Rossi <m.rossi@overside.it>
2023-03-25 23:57:15 +0100

    add increase power effect

--- power_bar/power_bar.gd
+++ power_bar/power_bar.gd
@@ -11,13 +11,13 @@ func _ready() -> void:
 	card_events.card_played.connect(_on_card_played)
 	card_events.card_returned.connect(_on_card_returned)
 	turn_manager_resource.restore_phase_started.connect(_on_restore_phase_started)
+	power_resource.max_power_changed.connect(_on_max_power_changed)
 	
 	for i in range(power_resource.max_power):
 		var instance = power_scene.instantiate()
 		add_child(instance)
 		instance.position = Vector2(i * 48, 0)
 		items.append(instance)
-	
 
 func _on_card_played(card: Card) -> void:
 	var cost = card.resource.cost
@@ -42,4 +42,12 @@ func _on_restore_phase_started() -> void:
 	power_resource.reset_power()
 	for item in items:
 		item.set_enabled(true)
+
+func _on_max_power_changed(num_power):
+	for i in range(items.size(), num_power):
+		var instance = power_scene.instantiate()
+		add_child(instance)
+		instance.position = Vector2(i * 48, 0)
+		instance.set_enabled(false)
+		items.append(instance)
 	
