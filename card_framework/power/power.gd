3ec20cd820f077bd0ef55c65c9c6ca8a9dbf283f
Michele Rossi <mikymod@gmail.com>
2023-12-25 00:53:06 +0100

    cleanup

--- card_framework/power/power.gd
+++ card_framework/power/power.gd
@@ -4,22 +4,8 @@ extends Control
 
 @onready var _label: Label = $TextureRect/CenterContainer/Label
 
-func _ready() -> void:
-	state.max_power_changed.connect(_on_max_power_changed)
-
 func _process(delta: float) -> void:
 	_label.text = "%s/%s" % [state.current_power, state.max_power]
 
-func _on_card_played(card: Card) -> void:
-	var cost = card.card_resource.cost
-	state.decrease_power(cost)
-
-func _on_card_returned(card: Card) -> void:
-	var cost = card.card_resource.cost
-	state.increase_power(cost)
-
-func _on_restore_phase_started() -> void:
+func restore_power() -> void:
 	state.reset_power()
-
-func _on_max_power_changed(num_power):
-	pass
