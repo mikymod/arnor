602e8a2ba4d40b6966dccfb4e08cafecbc312473
Michele Rossi <mikymod@gmail.com>
2024-02-09 01:22:53 +0100

    manage fires in building

--- buildings/building.gd
+++ buildings/building.gd
@@ -3,43 +3,22 @@ extends Area2D
 
 ## Represents a faction's building.
 
-signal building_harmed()
-signal building_destroyed()
+signal health_changed(health)
+signal destroyed()
 
 @export_group("Stats")
 ## The health of the building.
-@export var health: float = 1000
+@export var max_health: float = 1000
+@export var health: float = max_health
 
 @onready var fires = $Fires.get_children()
 var fires_index = 0
 
-var harm_step: int = health / fires.size()
-# fires_index = fmod(health, harm_step)
-
 func harm(amount: float) -> void:
 	health -= amount
-	building_harmed.emit()
+	health_changed.emit(health)
+	print(health)
 	if health <= 0:
-		destroy()
-
-## Spawns a fire
-func spawn_fire() -> void:
-	var fire: Fire = fires[fires_index]
-	fire.start()
-	if fires_index < fires.size() - 1:
-		fires_index += 1
-
-## Extinguish a fire
-func extinguish_fire() -> void:
-	if fires_index >= 0:
-		var fire: Fire = fires[fires_index]
-		fire.stop()
-		fires_index -= 1
-
-## Destroy the building
-func destroy() -> void:
-	building_destroyed.emit()
-	print ("building destroyed")
-
-
-
+		destroyed.emit()
+		$DestroyedSprite2D.visible = true
+		$AnimatedSprite2D.visible = false
--- buildings/fire/fire.gd
+++ buildings/fire/fire.gd
@@ -1,11 +1,26 @@
 class_name Fire
 extends Node2D
 
+@export var building: Building
+@export var threshold: int = 0
+
 var active: bool:
 	get:
 		return visible == true
 
+
+func _on_building_health_changed(health: float) -> void:
+	if health < threshold:
+		start()
+	else:
+		stop()
+
+
+func _on_building_destroyed() -> void:
+	stop()
+
 func start() -> void:
+	if active: return
 	visible = true
 	var scale_factor = randf_range(0.5, 1.2)
 	scale = Vector2(scale_factor, scale_factor)
