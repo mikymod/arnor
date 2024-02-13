eda973c0304805a0c37dbd58c498f5e878ab6d74
Michele Rossi <mikymod@gmail.com>
2024-02-13 07:54:27 +0100

    revamp power bar wip

---
+++ card_framework/power/power_bar/power_bar.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@export var state: CardBoardState = preload("res://card_framework/card_board_state.tres")
+
+@onready var _rects: Array[TextureRect] = [
+	$HBoxContainer/TextureRect,
+	$HBoxContainer/TextureRect2,
+	$HBoxContainer/TextureRect3,
+	$HBoxContainer/TextureRect4,
+	$HBoxContainer/TextureRect5,
+]
+
+func _process(_delta: float) -> void:
+	
+	update_ui()
+
+func update_ui() -> void:
+	for rect in _rects:
+		rect.modulate = rect.modulate.darkened(0.5)
+	for i in range(state.current_power):
+		_rects[i].modulate = _rects[i].modulate.lightened(0.5)
+
+func _on_card_played(card: Card) -> void:
+	var cost = card.card_resource.cost
+	state.decrease_power(cost)
