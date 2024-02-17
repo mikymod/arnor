9942280e8496860bc52bd541c84ee08a92b0332a
Michele Rossi <mikymod@gmail.com>
2024-02-17 12:41:37 +0100

    stop power bar progrression when encounter finishes

--- game_ui/power_bar/power_bar.gd
+++ game_ui/power_bar/power_bar.gd
@@ -1,10 +1,12 @@
 extends Control
 
-#@export var state: CardBoardState = preload("res://card_framework/card_board_state.tres")
 @export var supply: Supply = preload("res://supplies/gold.tres")
 @export var speed: float = 3
 
+var active: bool = true
+
 func _process(delta: float) -> void:
+	if not active: return
 	supply.increase(delta * 3)
 	$TextureProgressBar.value = supply.value
 
@@ -12,3 +14,5 @@ func _on_card_played(card: Card) -> void:
 	var cost = card.card_resource.cost
 	supply.decrease(cost * 10)
 
+func stop() -> void:
+	active = false
