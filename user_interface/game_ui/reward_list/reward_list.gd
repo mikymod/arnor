a0c74ecfa70c3c4412a8944a2f1229d3eb31b477
Michele Rossi <mikymod@gmail.com>
2024-03-10 12:24:20 +0100

    add reward area

--- user_interface/game_ui/reward_list/reward_list.gd
+++ user_interface/game_ui/reward_list/reward_list.gd
@@ -13,10 +13,6 @@ func _ready() -> void:
 	# FIXME: test, should be removed
 	add_gold_reward(139)
 	add_exp_reward(650)
-	add_gold_reward(139)
-	add_exp_reward(650)
-	add_gold_reward(139)
-	add_exp_reward(650)
 
 func add_gold_reward(amount: int) -> void:
 	var button = button_scene.instantiate() as RewardButton
@@ -27,7 +23,3 @@ func add_exp_reward(amount: int) -> void:
 	var button = button_scene.instantiate() as RewardButton
 	button.init(exp_reward_icon, exp_reward_text % amount)
 	$VBoxContainer.add_child(button)
-
-func add_card_reward(card: CardResource) -> void:
-	# TODO: manage this
-	pass
