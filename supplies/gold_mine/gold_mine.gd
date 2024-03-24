5a19e0ec20ad3cc2177ec658e80c77b0f1589d49
Michele Rossi <mikymod@gmail.com>
2024-03-24 09:21:02 +0100

    move all gathering logic inside pawn

--- supplies/gold_mine/gold_mine.gd
+++ supplies/gold_mine/gold_mine.gd
@@ -5,15 +5,12 @@ extends Area2D
 
 @export var supply_scene: PackedScene
 
-@onready var _gathering_bar: GatheringBar = $GatheringBar
-
 var _pawn: Pawn
 
 ## Activates the gold mine
 func activate() -> void:
 	$InactiveSprite.visible = false
 	$ActiveSprite.visible = true
-	_gathering_bar.start()
 
 ## Deactivates the gold mine
 func deactivate() -> void:
@@ -32,18 +29,14 @@ func destroy() -> void:
 func get_gather_position() -> Node2D:
 	return $GatherPosition
 
+func get_supply_position() -> Node2D:
+	return $SupplyPosition
+
 ## A callback invoked when a pawn entered the supply Area
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Pawn:
-		_pawn = body as Pawn
-		_gathering_bar.gathering_stopped.connect(_pawn.on_gathering_timeout)
 		activate()
 
-func _on_gathering_bar_gathering_stopped():
-	var supply = supply_scene.instantiate()
-	supply.position = $SupplyPosition.position
-	add_child(supply)
-	_pawn.player_resource.increase_mana(10)
-	_gathering_bar.gathering_stopped.disconnect(_pawn.on_gathering_timeout)
+func _on_gathering_bar_gathering_stopped() -> void:
 	deactivate()
-	_pawn = null
+
--- units/pawn/pawn.gd
+++ units/pawn/pawn.gd
@@ -1,7 +1,10 @@
 class_name Pawn
 extends Unit
 
-@onready var _gathering_progress: GatheringBar = $GatheringBar
+@onready var _gathering_bar: GatheringBar = $GatheringBar
+@export var supply_scene: PackedScene = preload("res://supplies/gold_spawn.tscn")
+
+var _gold_mine: GoldMine
 
 func cut() -> void:
 	state_machine.transition_to("Cut")
@@ -9,16 +12,29 @@ func cut() -> void:
 func mine() -> void:
 	state_machine.transition_to("Mine")
 
-func _on_gold_mine_reached() -> void:
-	agent.navigation_finished.disconnect(_on_gold_mine_reached)
-	mine()
-
 func _on_interaction_area_entered(area: Area2D) -> void:
 	if area is GoldMine:
-		var gold_mine = area as GoldMine
-		var gather_pos = gold_mine.get_gather_position()
+		_gold_mine = area as GoldMine
+		var gather_pos = _gold_mine.get_gather_position()
 		agent.target_position = gather_pos.global_position
 		agent.navigation_finished.connect(_on_gold_mine_reached)
 
-func on_gathering_timeout():
+func _on_gold_mine_reached() -> void:
+	agent.navigation_finished.disconnect(_on_gold_mine_reached)
+	_gathering_bar.gathering_stopped.connect(on_gathering_stopped)
+	_gathering_bar.start()
+	mine()
+
+func on_gathering_stopped():
+	player_resource.increase_mana(10)
+	_gathering_bar.gathering_stopped.disconnect(on_gathering_stopped)
+	var supply = supply_scene.instantiate()
+	supply.position = _gold_mine.get_supply_position().position
+	_gold_mine.add_child(supply)
 	state_machine.transition_to("Walk")
+
+func _exit_tree():
+	if agent.navigation_finished.is_connected(_on_gold_mine_reached):
+		agent.navigation_finished.disconnect(_on_gold_mine_reached)
+	if _gathering_bar.gathering_stopped.is_connected(on_gathering_stopped):
+		_gathering_bar.gathering_stopped.disconnect(on_gathering_stopped)
